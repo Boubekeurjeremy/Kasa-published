4beae8f5f6bb7e7b952c470cd8f6c7a7e81607bb
Jérémy Boubekeur <102968019+Boubekeurjeremy@users.noreply.github.com>
2024-02-18 06:48:13 +0100

    Kasa online

--- kasa/src/pages/Error/index.jsx
+++ kasa/src/pages/Error/index.jsx
@@ -16,4 +16,43 @@ function Error() {
     );
 }
 
+
+/*import { Link } from "react-router-dom";
+import IMG from "../../assets/IMG.png";
+import Card from "../../components/Cards";
+import "../../styles/css/prefixed/style.css";
+import { housingList } from "../../datas/housingList";
+
+function Error() {
+    return (
+        <div>
+            <div className="kasa__home">
+                <img
+                    src={IMG}
+                    alt="Paysage d'une forêt"
+                    className="kasa__home__image"
+                />
+                <h2 className="kasa__home__text">
+                    Chez vous, partout et ailleurs
+                </h2>
+            </div>
+            <div className="kasa__card">
+                {housingList.map(({ id, cover, title }) => (
+                    <Link
+                        key={id}
+                        to={"/housing/" + id + "/#"}
+                        className="kasa__card__link"
+                    >
+                        <Card
+                            key={`${cover}-${id}`}
+                            cover={cover}
+                            title={title}
+                        />
+                    </Link>
+                ))}{" "}
+            </div>
+        </div>
+    );
+}*/
+
 export default Error;
